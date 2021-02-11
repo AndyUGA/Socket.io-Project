63e2d5fe6757a7d7c5552803242653d43959d567
Andy Truong <att35663@uga.edu>
2021-02-10 22:26:51 -0500

    Added global variable

--- server.js
+++ server.js
@@ -10,9 +10,29 @@ const io = socketio(server);
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
+let totalAmount = 3000;
+
 io.on('connection', socket => {
-    console.log("New connection!");
+
+    //Emits to a new client
+    socket.emit('message', `The total amount is ${totalAmount}`);
+
+    //Emits to all existing clients besdies current client
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    //Emits to all existing clients and current client
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    })
+
+    socket.on('chatMessage', (msg) => {
+
+        totalAmount -= msg;
+        console.log(`Total amount is ${totalAmount}`);
+    });
+
 });  
+
 
 
 const PORT = 3000 || process.env.PORT;
